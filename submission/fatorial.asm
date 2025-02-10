f18b347e7ab7c8335f4c5786530804e9a7765506
Henrique Maruiti <149033733+Maruitih@users.noreply.github.com>
2025-02-10 10:59:26 -0300

    Update fatorial.asm

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -1,15 +1,16 @@
-LD  N
-JN /012
-LD RES
-ML N
-MM RES
-LD N
-SB T
-MM N
-JP /000
-HM /012
+;Fatorial
+K /8100 ; AC = N
+K /1012 ; desvio para endereco de parada
+K /8102 ; AC = resultado
+K /6100 ; AC = resultado * N
+K /9102 ; Load no valor de resultado
+K /8100 ; AC = N
+K /5104 ; N = N - 1
+K /9100 ; Load no valor de N
+K /0000 ; Desvio incondicional
+K /C012 ; Endereco de parada
 
-@ /100
-N K /0120
-RES K /0001
-T K /0001
+@ /0100
+N K /120 ; Valor de N
+RES K /0001 ; Valor do resultado
+T K /0001 ; Usado para N-1
