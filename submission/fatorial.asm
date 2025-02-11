0f5327c8d64dd0a9bb80387d0aa0775f74771ac8
Henrique Maruiti <149033733+Maruitih@users.noreply.github.com>
2025-02-10 22:36:10 -0300

    Update fatorial.asm

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -1,16 +1,26 @@
-;Fatorial
-LD /100 ; AC = N
-JZ /012 ; desvio para endereco de parada
-LD /102 ; AC = resultado
-ML /100 ; AC = resultado * N
-MM /102 ; Load no valor de resultado
-LD /100 ; AC = N
-SB /104 ; N = N - 1
-MM /100 ; Load no valor de N
-JP /000 ; Desvio incondicional
-HM /012 ; Endereco de parada
+@ /000
+LD N ; ac = n  
+JZ /f00 ; termina o programa se n = 0
+SC /300 ; 
+HM /a00 ;
 
-@ /0100
-N K /120 ; Valor de N
-RES K /0001 ; Valor do resultado
-T K /0001 ; Usado para N-1
+@ /100
+N K /004
+RES K /001
+CTE K /001
+AUX K /001
+
+@ /300
+ML RES; AC = N * res 
+MM RES; RES = N * res
+LD N; ac = N
+SB CTE; ac = N - 1
+MM N ; N = ac
+JZ /400; quebra o loop para N = 0 
+JP /300; volta para o loop, se N !=0
+
+@ /400
+RS /006 ; quebra a sub
+
+@ /f00 ; f00
+HM /a00
